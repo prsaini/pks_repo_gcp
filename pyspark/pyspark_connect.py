7a81c49390ccca61b85a354d085098b914d19842
Praveen <p.saini1506@gmail.com>
2025-07-22 21:32:40 -0500

    adk basics

--- pyspark/pyspark_connect.py
+++ pyspark/pyspark_connect.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName("MySparkApp").getOrCreate()
+spark = SparkSession.builder.appName("MySparkApp").master("local[*]").getOrCreate()
 data = [(1, "Alice"), (2, "Bob")]
 df = spark.createDataFrame(data, ["id", "name"])
 df.show()
